9bb369d5ade094de897f8704f66753e450ed3a71
Leonardo Kammer <leokammer@aim.com>
2017-04-09 17:37:16 -0400

    Initial structure of the test files and hexa49 library

---
+++ site/site.js
@@ -0,0 +1,14 @@
+$("svgElement").ready(function () {
+  'use strict';
+  svgdraw();
+});
+
+document.getElementById("site-footer").innerHTML =
+  "<p>&copy;  " + new Date().getFullYear() + " Kammer Sites.</p>";
+
+//document.getElementById("nav01").innerHTML =
+//"<ul id='menu'>" +
+//"<li><a href='index.html'>Home</a></li>" +
+//"<li><a href='customers.html'>Data</a></li>" +
+//"<li><a href='about.html'>About</a></li>" +
+//"</ul>";
